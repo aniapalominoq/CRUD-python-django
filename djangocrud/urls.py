00ccf54a49d03175efb3d095f1bd8c885c6e87ea
Ania pq <melanpq@gmail.com>
2023-05-07 02:12:35 -0500

    implementacion del navegador

--- djangocrud/urls.py
+++ djangocrud/urls.py
@@ -23,6 +23,6 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('signup/', views.signup, name='signup'),
     path('notes/', views.notes, name='notes'),
-    path('logout/', views.signout, name='signout'),
+    path('logout/', views.signout, name='logout'),
     path('signin/', views.signin, name='signin'),
 ]
